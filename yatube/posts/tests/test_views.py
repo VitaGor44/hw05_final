0e37c035f50dfebe19f8322d140dda9b2bbddc95
VitaGor44 <95421322+VitaGor44@users.noreply.github.com>
2022-11-24 13:28:30 +0700

    Change my tesy_views in PEP8

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -5,7 +5,6 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from django import forms
 from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.cache import cache
 
 from ..models import Group, Post, User, Follow
@@ -16,19 +15,20 @@ class PostPagesTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         settings.MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
-        small_gif = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
+        # small_gif = (
+        #     b'\x47\x49\x46\x38\x39\x61\x02\x00'
+        #     b'\x01\x00\x80\x00\x00\x00\x00\x00'
+        #     b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+        #     b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+        #     b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+        #     b'\x0A\x00\x3B'
+        # )
         # uploaded = SimpleUploadedFile(
         #     name='small.gif',
         #     content=small_gif,
         #     content_type='image/gif'
         # )
+
 
         cls.user = User.objects.create_user(username='auth')
         cls.group = Group.objects.create(
